457166f12baa1a227c5815d318be9699e5ac7cb6
AlexGz9851 <a00820225@itesm.mx>
2019-02-01 22:49:00 -0600

    1stForceField
    I already make my first vectorial force field, changing gravity into space, as i want.

---
+++ Assets/Scripts/CharaMovment.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CharaMovment : MonoBehaviour
+{
+    public int speed=3;
+    public float power;
+    float h, v,u;
+    public Rigidbody rb;
+    private Transform t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        power = 3;
+        rb= GetComponent<Rigidbody>();
+        t = GetComponent<Transform>();
+}
+    
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        u = Input.GetAxis("Jump");
+        transform.Translate(speed * Time.deltaTime*h,0, speed * Time.deltaTime * v, Space.World);
+        //print(u);
+       
+        
+    }
+    public void OnCollisionStay(Collision collision)
+    {
+        //print(collision.transform.name);
+        if (u!=0)
+        {
+            rb.AddForce(0, power * u, 0, ForceMode.Impulse);
+        }
+
+    }
+
+}
+
---
+++ Assets/Scripts/FieldOfForces.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfForces : MonoBehaviour
+{
+    Rigidbody rb;
+    public float fieldPower;
+    private Transform t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = GetComponent<Transform>();
+        fieldPower = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("FColliding with " + collision.transform.name);
+    }
+
+    // This methods is invoked during the collision
+    // When they stop touching
+    private void OnCollisionExit(Collision collision)
+    {
+        print("FExit");
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("FTrigger Enter");
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        rb = other.GetComponent<Rigidbody>();
+        rb.AddForce(0.2f * fieldPower * t.position.z, 0.5f * fieldPower * t.position.x, 0.2f * fieldPower * t.position.x, ForceMode.Acceleration);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        print("FTrigger exit");
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+
+    }
+
+}
